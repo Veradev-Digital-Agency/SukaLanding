5308b538b4615c7c2eded902513242cefd744282
Alan Zagi <alanzagi2007@gmail.com>
2025-07-04 22:13:44 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,10 +10,69 @@
     </head>
 
     <body>
-        <div class="container mx-auto">
-            <h1 class="text-3xl font-bold">Welcome to My Website</h1>
-            <p class="mt-4">This is a simple page using Tailwind CSS and Flowbite.</p>
-            <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+        <nav class="bg-white border-gray-200 fixed top-0 left-0 w-full z-50">
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap text-black">Flowbite</span>
+                </a>
+                <button data-collapse-toggle="navbar-default" type="button"
+                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                    aria-controls="navbar-default" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M1 1h15M1 7h15M1 13h15" />
+                    </svg>
+                </button>
+                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                    <ul
+                        class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+                        <li>
+                            <a href="#"
+                                class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0"
+                                aria-current="page">Home</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">About</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Services</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Pricing</a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="mt-16 relative text-white">
+            <div class="absolute inset-0 bg-fixed bg-center bg-cover z-0"
+                style="background-image: url('{{ asset('storage/laptop-background.jpeg') }}');">
+            </div>
+
+            <!-- Overlay Gelap -->
+            <div class="absolute inset-0 bg-black opacity-60 z-10"></div>
+
+            <div class="relative px-6 space-y-8 py-20 pr-20 z-20">
+                <h2>we are trusted our customers</h2>
+                <h1>We Provide Experts To
+                    Great Value For Business</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis voluptas ducimus error
+                    explicabo.
+                    Nihil sit pariatur quis et ea error labore ab laborum, aliquid repellat.</p>
+                <a href="">Contact Us</a>
+            </div>
+        </main>
     </body>
 
 </html>
